109e8d28c5612f0d1735ac2185968e3dc6601be7
Amr Tamer <114962896+AmrTamerDev@users.noreply.github.com>
2025-02-18 02:45:56 +0200

    Implicity Typed Local Variable [var, dynamic]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Implicity Typed Local Variable [var, dynamic]
+            #region Var
+            //var Name = "Ahmed";
+            //Compiler will detect the variable data type
+            //with initialize value in this variable
+            //var Name02; //Invalid
+            //var Name03 = null; //Invalid
+            //Name = null; //valid
+            #endregion
+
+            #region Dynamic
+            //dynamic name = "Ahmed";
+            //name = 10;
+            //name = true;
+
+            //dynamic X; // not must be initialize
+            //dynamic y = null; // can be initialize with null
+            //CLR will detect the variable datatype
+            //Based on it's last assigned value in runtime
+            #endregion
+
+            #endregion
+            
+
         }
     }
 }
